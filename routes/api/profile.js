3921ac3d4dc47ed545bc21bd8b5fad500f1feeef
Yeshu <2000yeshu@gmail.com>
2020-05-05 03:37:00 +0530

    github api endpoint access fix

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -302,11 +302,15 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 router.get("/github/:username", async (req, res) => {
   try {
     const options = {
-      uri: `https://api.github.com/users/${
+      //Using OAuth is deprecated
+      /*uri: `https://api.github.com/users/${
         req.params.username
       }/repos?per_page=5&sort=created:asc&client_id=${config.get(
         "githubClientId"
-      )}&client_secret=${config.get("githubSecret")}`,
+      )}&client_secret=${config.get("githubSecret")}`,*/
+
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`,
+
       method: "GET",
       headers: { "User-agent": "node-js" },
     };
