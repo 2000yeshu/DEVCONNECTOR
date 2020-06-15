c513ee0d84dbad35f4d6b68bc62f08dc7ed97d98
Yeshu <2000yeshu@gmail.com>
2020-06-15 21:26:07 +0530

    Ready for Deployment

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 var bodyParser = require("body-parser");
+const path = require("");
 
 //connect db
 connectDB();
 
-app.get("/", (_, response) => response.send("API running"));
 app.use(bodyParser.json());
 
 //routes
@@ -14,6 +14,16 @@ app.use("/api/users", require("./routes/api/user"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
+
+//Serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("/client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 7000;
 
