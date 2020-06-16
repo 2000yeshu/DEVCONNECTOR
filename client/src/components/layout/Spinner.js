f6029a6e873cb17acfd66d179877e3b5b886541e
Yeshu <2000yeshu@gmail.com>
2020-06-17 01:55:07 +0530

    spinner

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -5,7 +5,7 @@ export default () => {
   return (
     <Fragment>
       <img
-        src="https://giphy.com/gifs/mashable-3oEjI6SIIHBdRxXI40"
+        src={"https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"}
         style={{ width: "200px", margin: "auto", display: "block" }}
         alt="Loading..."
       />
