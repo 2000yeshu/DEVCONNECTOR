24bd7ea10b2239e7cba1f19001c55a3682ae2b68
Yeshu <2000yeshu@gmail.com>
2020-06-17 02:36:19 +0530

    resolved imports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 var bodyParser = require("body-parser");
-const path = require("");
+const path = require("path");
 
 //connect db
 connectDB();
