279c96723d4ec60c1b950865d2cfb39d5327f35b
Yeshu <2000yeshu@gmail.com>
2020-06-17 02:03:07 +0530

    production env resolved

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use("/api/profile", require("./routes/api/profile"));
 
 //Serve static assests in production
 if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("/client/build"));
+  // Set static folder
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
